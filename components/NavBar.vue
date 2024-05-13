4331bb6a2282bbe9fcf8ef3ba5d7e8bdb9cc28f0
SofDob99 <153644690+SofDob99@users.noreply.github.com>
2024-05-13 20:05:05 +0200

    first commit

---
+++ components/NavBar.vue
@@ -0,0 +1,46 @@
+<template>
+    <nav>
+        <ul>
+    
+        </ul>
+        <div class="user" @click="panelActive = !panelActive">u</div>
+        <div class="panel" v-show="panelActive">
+        </div>
+
+
+    </nav>
+</template>
+
+<style lang="postcss">
+nav {
+    background-color: bisque;
+    
+
+}
+ul {
+    @apply flex p-4;
+
+    li {
+        @apply p-4;
+    }
+}
+
+.user {
+    margin-left: 30px
+
+}
+
+.panel {
+    width: 30px;
+    height: 30px;
+    background-color: aqua
+}
+.cuerpo {
+    margin: 30px;
+}
+</style>
+
+<script setup>
+import { ref } from 'vue'
+const panelActive = ref(false)
+</script>
---
+++ pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+      <h1>holi</h1>
+    </div>
+  </template>
+  
